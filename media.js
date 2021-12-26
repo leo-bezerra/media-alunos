a26f4f85f91110206bd018afbcc3f8211994f807
leo-bezerra <tubzleo@gmail.com>
2021-12-26 20:18:51 -0300

    estudo sistema média alunos

---
+++ media.js
@@ -0,0 +1,29 @@
+/* SISTEMA DE MÉDIA POR ALUNO
+let media1 = 1
+let media2 = 7
+let media3 = 7
+
+let mediaTotal = (media1 + media2 + media3)/3
+
+if (mediaTotal >=7) {
+    console.log("Parabéns, você foi aprovado!")
+} else {
+    console.log("Você não atingiu a média, reprovado!")
+}*/
+
+function media(){
+    let nota1 = parseFloat(document.getElementById("nota1").value)
+    let nota2 = parseFloat(document.getElementById("nota2").value)
+    let nota3 = parseFloat(document.getElementById("nota3").value)
+
+    let media = (nota1 + nota2 + nota3)/3
+
+    if (media >= 7)
+     if(media==10)
+      alert("Parabéns, você foi aprovado com média máxima: " +media)
+     else
+      alert("Parabéns, você foi aprovado com média: " +media)
+    else
+     alert("Reprovado! Você não antingiu o média necessária.")
+
+   }
